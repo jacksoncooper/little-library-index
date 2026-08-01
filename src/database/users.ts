7e5c8f382c25a672b99c2aa48e96583bfe47eda0
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2026-08-01 16:25:46 -0700

    Implement `readUser`.

--- src/database/users.ts
+++ src/database/users.ts
@@ -13,20 +13,34 @@ type User = {
 
 async function createUser(db: SQL, handle: string): Promise<number> {
     const rows = await db<Row[]>`
-        INSERT INTO users ( handle )
+        INSERT INTO users (handle)
         VALUES (${handle})
         RETURNING id
     `;
-    assertRowCount(rows, 1);
-    const column = rows[0];
-    assertColumn(column, 'id', 'number');
-    return column.id;
+    assertRowCount(rows, 1); // Only one row was inserted.
+    const row = rows[0];
+    assertColumn(row, 'id', 'number');
+    return row.id;
 }
 
 // Used to handle a request for a user profile. For example,
 //
 //   https://littlelibraryindex.com/user/jackson
 //
-function readUser(db: SQL, handle: string): Promise< WithPrimaryKey< User > > {
-    return Promise.resolve( { id: 1, handle: 'jackson' } );
+async function readUser(
+    db: SQL,
+    handle: string
+): Promise<WithPrimaryKey<User> | null> {
+    const rows = await db<Row[]>`
+        SELECT id, handle FROM users
+        WHERE handle = ${handle}
+    `;
+    if (rows.length < 1) {
+        return null;
+    }
+    assertRowCount(rows, 1); // The 'handle' column is unique.
+    const row = rows[0];
+    assertColumn(row, 'id', 'number');
+    assertColumn(row, 'handle', 'string');
+    return { id: row.id, handle: row.handle };
 }
