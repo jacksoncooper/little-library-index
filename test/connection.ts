b2cb1db2ddcd005946eaea4c2c6f08fdc38a14a2
Jackson Cooper <jacksoncooper@users.noreply.github.com>
2026-08-02 08:24:35 -0700

    Move test database connection handling to its own module.

--- test/connection.ts
+++ test/connection.ts
@@ -8,3 +8,34 @@ export function testConnection(): SQL {
         database: testDatabaseName,
     });
 }
+
+export async function createTestDatabase(name: string): Promise<$.ShellOutput> {
+    // An unusual design choice of Bun's shell API is that Promises constructed
+    // with `$` do not execute until awaited.
+    //
+    //   By default, a non-zero exit code throws an error.
+
+    // TODO: These arguments to `createdb` may not be portable. They're
+    // definitely not potable.
+    return ($`\
+        createdb \
+            --locale-provider=icu --icu-locale=und --template=template0 \
+            ${name}
+        psql ${name} --quiet --file=../database/schema.sql`
+        .cwd(import.meta.dir)
+    );
+}
+
+export async function deleteTestDatabase(name: string): Promise<$.ShellOutput> {
+    return $`dropdb ${name}`;
+}
+
+export function withDatabaseConnection<T>(
+    db: SQL, query: (db: SQL) => Promise<T>
+): Promise<T> {
+    return query(db).then(
+        result => result 
+    ).finally(
+        () => db.close()
+    )
+}
--- test/users-schema.test.ts
+++ test/users-schema.test.ts
@@ -4,28 +4,13 @@ import { afterEach, beforeEach, expect, test } from 'bun:test';
 import { Row } from '../src/database/types';
 import { readUser } from '../src/database/users';
 
-import { testConnection, testDatabaseName } from './connection';
-
-async function createTestDatabase(name: string): Promise<$.ShellOutput> {
-    // An unusual design choice of Bun's shell API is that Promises constructed
-    // with `$` do not execute until awaited.
-    //
-    //   By default, a non-zero exit code throws an error.
-
-    // TODO: These arguments to `createdb` may not be portable. They're
-    // definitely not potable.
-    return ($`\
-        createdb \
-            --locale-provider=icu --icu-locale=und --template=template0 \
-            ${name}
-        psql ${name} --quiet --file=../database/schema.sql`
-        .cwd(import.meta.dir)
-    );
-}
-
-async function deleteTestDatabase(name: string): Promise<$.ShellOutput> {
-    return $`dropdb ${name}`;
-}
+import {
+    createTestDatabase,
+    deleteTestDatabase,
+    testConnection,
+    testDatabaseName,
+    withDatabaseConnection
+} from './connection';
 
 beforeEach(async () =>
     createTestDatabase(testDatabaseName)
@@ -39,15 +24,6 @@ afterEach(async () =>
     deleteTestDatabase(testDatabaseName)
 );
 
-function withDatabaseConnection<T>(
-    db: SQL, query: (db: SQL) => Promise<T>
-): Promise<T> {
-    return query(db).then(
-        result => result 
-    ).finally(
-        () => db.close()
-    )
-}
 async function writeUsers(db: SQL): Promise<Row[]> {
     // Similar to Promises constructed with `$`, the query will not execute
     // until the promise is awaited.
